fc1e8fe6e303596b475e240445af3c2aeade583c
Tejesh Mehta <tejesh.mehta@gmail.com>
2021-05-30 23:15:42 -0700

    allow signal to be nullish

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import raceAbort, { AbortError } from 'race-abort';
+import raceAbort, { AbortError } from 'race-abort'
+
 import timeout from 'timeout-then'
 
-export { AbortError } from 'race-abort';
+export { AbortError } from 'race-abort'
 
-export default async function abortableTimeout(duration: number, signal: AbortSignal): Promise<void> {
+export default async function abortableTimeout(
+  duration: number,
+  signal: AbortSignal | null | undefined,
+): Promise<void> {
   const timer = timeout(duration)
   try {
-    await raceAbort(signal, timer)
-  } catch(err) {
+    if (signal == null) {
+      await timer
+    } else {
+      await raceAbort(signal, timer)
+    }
+  } catch (err) {
     if (err instanceof AbortError) timer.clear()
     throw err
   }
